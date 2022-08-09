e01e9376f0d3a27e0b46e55887daebd12e04d449
Gokhan AYNALI <gokhanaynali@outlook.com>
2022-08-09 22:26:57 +0200

    HelloGitHub package is updated with new codes

--- src/aynaliPractice/HelloGitHub.java
+++ src/aynaliPractice/HelloGitHub.java
@@ -3,5 +3,18 @@ package aynaliPractice;
 public class HelloGitHub {
     public static void main(String[] args) {
         //testing
+        // there are new fancy codes written by me.
+        // there are new fancy codes written by me.
+        // there are new fancy codes written by me.
+        // there are new fancy codes written by me.
+        // there are new fancy codes written by me.
+        // there are new fancy codes written by me.
+        // there are new fancy codes written by me.
+        // there are new fancy codes written by me.
+        // there are new fancy codes written by me.
+        // there are new fancy codes written by me.
+        // there are new fancy codes written by me.
+        // there are new fancy codes written by me.
+        // there are new fancy codes written by me.
     }
 }
