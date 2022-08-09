d361f966d36828244399ebcbfb4e5d6da0149657
musty <mstfkvk5600@gmail.com>
2022-08-09 23:25:54 +0300

    second commit

---
+++ src/conflict/Conflict.java
@@ -0,0 +1,9 @@
+package conflict;
+
+public class Conflict {
+    public static void main(String[] args) {
+        for (int i = 0; i <10 ; i++) {
+            System.out.println("musty");
+        }
+    }
+}
